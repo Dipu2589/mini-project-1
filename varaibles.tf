066849fb957092d160f9543514bac8298016f65d
Dipu2589 <prajapati.dipu@gmail.com>
2026-05-18 20:18:17 -0400

    updated the backend file

--- varaibles.tf
+++ varaibles.tf
@@ -7,11 +7,3 @@ variable "location" {
   description = "Default Location for mini1 project"
   type        = string
 }
-
-variable "tenant_id" {
-  description = "Main account tenant ID"
-}
-
-variable "client_id" {
-  description = "Main account Client ID"
-}
